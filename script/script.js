52d850efc7f33106f4f329527f965f18283a2f9b
natsitarz <natsitarz@icloud.com>
2022-11-18 23:36:01 +0100

    JavaScript date in footer

---
+++ script/script.js
@@ -0,0 +1,9 @@
+var data = new Date();
+
+var rok = data.getFullYear();
+
+function aktualnyrok() {
+    document.getElementById("year").innerHTML = "ECO® " + rok;
+}
+
+console.log("js file linked | should be working properly");
