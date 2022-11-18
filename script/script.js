4893f66a7eceee454c9914d93d22f156f911cbce
natsitarz <natsitarz@icloud.com>
2022-11-18 23:54:22 +0100

    js finished

--- script/script.js
+++ script/script.js
@@ -1,12 +1,14 @@
 //{Mozliwosc 1: }
 // var data = new Date();
 // var rok = data.getFullYear();
+//
+// document.getElementById("year").innerHTML = "ECO® " + rok
 ////////////////////////////////
 //{Mozliwosc 2: }
 // function rok() {
 //    return Date().getFullYear()
 //}
-// document.getElementById("year").innerHTML = `ECO ${rok()}`
+// document.getElementById("year").innerHTML = `ECO® ${rok()}`
 ////////////////////////////////
 
 //{Mozliwosc 3: }
