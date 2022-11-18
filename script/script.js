bc6441e80295de95e64566675caabae80ad1a99b
natsitarz <natsitarz@icloud.com>
2022-11-18 23:44:27 +0100

    Other possible ways in JS

--- script/script.js
+++ script/script.js
@@ -1,3 +1,13 @@
+//{Mozliwosc 1: }
+// var data = new Date();
+// var rok = data.getFullYear();
+////////////////////////////////
+//{Mozliwosc 2: }
+// function rok() {
+//    return Date().getFullYear()
+//}
+
+//{Mozliwosc 3: }
 function aktualnyrok() {
     document.getElementById("year").innerHTML = `ECO® ${new Date().getFullYear()}`
 }
