7400486ab352b8a24a11c7d7085e85d5a3da27e4
natsitarz <natsitarz@icloud.com>
2023-09-26 22:19:37 +0200

    better script

--- script/script.js
+++ script/script.js
@@ -39,27 +39,24 @@ const api = {
 
 async function checkWeather() {
     let userLocation = document.getElementById("user-location").value;
-    const search = () => {
-        fetch(api.decoder + userLocation + `&appid=${api.key}`)
-            .then(res => res.json())
-            .then(result => {
-                try {
-                    fetch(api.url + `lat=${result[0].lat}&lon=${result[0].lon}&units=metric&appid=${api.key}`)
-                        .then(result => result.json())
-                        .then(result => {
-                            console.log(result);
-                            let maindiv = document.getElementById("main-location").style;
-                            let nextdiv = document.getElementById("main-weather").style;
-                            document.getElementById("done-2").style.opacity = "0";
-                            document.getElementById("user-location").style.opacity = "0";
-                            document.getElementById("main-text-2").innerHTML = "Thanks!";
-                            divchange(maindiv, nextdiv);
-                        })
-                } catch (error) {
-                    console.log(error);
-                    document.getElementById("main-text-2").innerHTML = "Sorry, we couldn't find your location. Try again!";
-                }
-            })
+    const search = async() => {
+        let response = await fetch(api.decoder + userLocation + `&appid=${api.key}`)
+        let jsonResponse = await response.json();
+        console.log(jsonResponse);
+        if (jsonResponse.length == 0) {
+            document.getElementById("main-text-2").innerHTML = "Sorry, we couldn't find your location. Try again!";
+        } else {
+            let anotherResponse = await fetch(api.url + `lat=${jsonResponse[0].lat}&lon=${jsonResponse[0].lon}&units=metric&appid=${api.key}`)
+            let jsonAnotherResponse = await anotherResponse.json();
+            console.log(jsonAnotherResponse);
+            let maindiv = document.getElementById("main-location").style;
+            let nextdiv = document.getElementById("main-weather").style;
+            document.getElementById("done-2").style.opacity = "0";
+            document.getElementById("user-location").style.opacity = "0";
+            document.getElementById("main-text-2").innerHTML = "Thanks!";
+            divchange(maindiv, nextdiv);
+        }
     }
+
     search();
 }
